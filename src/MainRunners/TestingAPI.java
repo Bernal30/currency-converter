f566a376ed73fe45c7c908d9b245a5d0c172e9b4
Bernal30 <abrn7311@gmail.com>
2024-04-23 20:24:11 -0600

    wip - Adding a request from API

--- src/MainRunners/TestingAPI.java
+++ src/MainRunners/TestingAPI.java
@@ -1,7 +1,25 @@
 package MainRunners;
 
-public class TestingAPI {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
+public class TestingAPI {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        //creamos una request a un servidor mediante HTTP
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                //se coloca el link de la API request
+                .uri(URI.create("https://v6.exchangerate-api.com/v6/4f4f950220bf969d1cbfd78a/latest/mxn"))
+                .build();
+
+        //tenemos que resivir los datos del srevidor
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
     }
 }
